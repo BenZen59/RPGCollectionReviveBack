fea91a7d6d14cfeedece61422291e9add5489d58
BenZen59 <benoit.zenone@gmail.com>
2023-07-11 16:59:18 +0200

    add get rpg by id et update by id

--- rpgcollectionrevive/src/main/java/rpgcollectionrevive/rpgcollectionrevive/controller/RPGController.java
+++ rpgcollectionrevive/src/main/java/rpgcollectionrevive/rpgcollectionrevive/controller/RPGController.java
@@ -1,7 +1,9 @@
 package rpgcollectionrevive.rpgcollectionrevive.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import rpgcollectionrevive.rpgcollectionrevive.exception.ResourceNotFoundException;
 import rpgcollectionrevive.rpgcollectionrevive.model.RPG;
 import rpgcollectionrevive.rpgcollectionrevive.repository.RPGRepository;
 
@@ -23,5 +25,26 @@ public class RPGController {
     @PostMapping("/rpg")
     public RPG createRPG(@RequestBody RPG rpg){
         return rpgRepository.save(rpg);
+    }
+
+    //get rpg by id
+    @GetMapping("/rpg/{id}")
+    public ResponseEntity<RPG> getRPGById(@PathVariable Integer id){
+        RPG rpg = rpgRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("RPG not exist with id:" + id));
+        return ResponseEntity.ok(rpg);
+    }
+
+    //update RPG
+    @PutMapping("/rpg/{id}")
+    public ResponseEntity<RPG> updateRPG(@PathVariable Integer id, @RequestBody RPG rpgDetails){
+        RPG rpg = rpgRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("RPG not exist with id:" + id));
+        rpg.setName(rpgDetails.getName());
+        rpg.setSupport(rpgDetails.getSupport());
+        rpg.setGenre(rpgDetails.getGenre());
+        rpg.setDeveloper(rpgDetails.getDeveloper());
+        rpg.setImagerpg(rpgDetails.getImagerpg());
+
+        RPG updatedRPG = rpgRepository.save(rpg);
+        return ResponseEntity.ok(updatedRPG);
     }
 }
